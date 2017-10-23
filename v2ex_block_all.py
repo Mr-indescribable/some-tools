e2efa4c74b0c4fec0a7053eab223a5831a7fac67
Mr-indescribable <st.indescribable@gmail.com>
2017-10-23 11:25:15 +0800

    v2ex_block_all

---
+++ v2ex_block_all.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3.6
+#coding: utf-8
+
+import re
+import time
+
+import requests
+
+
+HOME = 'https://www.v2ex.com/?tab=all'
+
+
+class V2EXBlocker():
+
+    def __init__(self, cookies={}):
+        self._cookies = cookies
+
+    def __get(self, url):
+        return requests.get(url=url, cookies=self._cookies)
+
+
+    def block(self, uid):
+        url = 'https://www.v2ex.com/block/%d?t=1445741587' % uid
+        self.__get(url)
+        print('blocked UID=%d' % uid)
+
+    def block_all(self):
+        uid = 12876
+        while True:
+            try:
+                self.block(uid)
+            except:
+                pass
+            uid += 1
+            time.sleep(2)
+
+    @property
+    def home_page(self):
+        return self.__get(HOME).content.decode('utf-8')
+
+
+if __name__ == '__main__':
+    cookies = {}
+
+    vb = V2EXBlocker(cookies)
+    vb.block_all()

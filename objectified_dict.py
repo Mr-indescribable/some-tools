9f93cbe0360b2c997d4ff3aa541380dd66c7acd7
Mr-indescribable <st.indescribable@gmail.com>
2018-11-16 10:47:20 +0800

    update OD from ic

--- objectified_dict.py
+++ objectified_dict.py
@@ -7,58 +7,77 @@ import json
 class ObjectifiedDict():
 
     def __getattribute__(self, name):
-        try:
-            return object.__getattribute__(self, name)
-        except AttributeError:
-            return None
+        container = object.__getattribute__(self, '__container__')
+        if name in container:
+            return container.get(name)
+        else:
+            try:
+                return object.__getattribute__(self, name)
+            except AttributeError:
+                return None
 
-    @staticmethod
-    def _convert(item):
+    def __setattr__(self, name, value):
+        self.__update__(**{name: value})
+
+    def __convert__(self, item):
         if isinstance(item, dict):
             return ObjectifiedDict(**item)
         if isinstance(item, list):
-            return [ObjectifiedDict._convert(unit) for unit in item]
+            return [self.__convert__(unit) for unit in item]
         if isinstance(item, tuple):
-            return (ObjectifiedDict._convert(unit) for unit in item)
+            # this is necessary,
+            # by default, this tuple derivation will return a generator
+            return tuple(
+                (self.__convert__(unit) for unit in item)
+            )
         if isinstance(item, set):
-            return {ObjectifiedDict._convert(unit) for unit in item}
+            return {self.__convert__(unit) for unit in item}
         else:
             return item
 
     def __init__(self, **kwargs):
+        object.__setattr__(self, '__container__', dict())
         for key, value in kwargs.items():
-            self.__dict__[key] = self._convert(value)
+            a = self.__convert__(value)
+            self.__container__[key] = a
+
+    def __update__(self, **kwargs):
+        for key, value in kwargs.items():
+            self.__container__[key] = self.__convert__(value)
 
     def __iter__(self):
         return iter(
-            self.__dict__.items()
+            self.__container__.items()
         )
 
     def __bool__(self):
-        return bool(self.__dict__)
+        return bool(self.__container__)
 
     def __clear__(self):
-        self.__dict__.clear()
+        self.__container__.clear()
 
     @staticmethod
-    def _to_dumpable(item):
+    def __to_dumpable__(item, keep_bytes=True):
         if item.__class__ is bytes:
-            return '<bytes length=%d>' % len(item)
+            return item if keep_bytes else f'<bytes length={len(item)}>'
         elif isinstance(item, ObjectifiedDict):
-            return item.__str_assist__()
+            return item.__to_dict__(keep_bytes)
         elif item.__class__ in (list, tuple, set):
-            return [ObjectifiedDict._to_dumpable(unit) for unit in item]
+            return [ObjectifiedDict.__to_dumpable__(unit) for unit in item]
+        elif item.__class__ == bool:
+            return item
         elif item.__class__ not in (int, str, None):
             return str(item)
+
         return item
 
-    def __str_assist__(self):
+    def __to_dict__(self, keep_bytes=True):
         d = {}
-        for key, value in self.__dict__.items():
-            d[key] = self._to_dumpable(value)
+        for key, value in self.__container__.items():
+            d[key] = self.__to_dumpable__(value, keep_bytes)
         return d
 
     def __str__(self):
         return json.dumps(
-            self.__str_assist__(), indent=4
+            self.__to_dict__(keep_bytes=False), indent=4
         )
